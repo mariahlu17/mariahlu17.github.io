828f6f48e48c8a2c0059efe14479849c94d00baf
mariahlu17 <62620965+mariahlu17@users.noreply.github.com>
2022-07-06 13:34:10 +0200

    Update script.js
    updating js code

--- script.js
+++ script.js
@@ -9,7 +9,14 @@ const answerThird = document.querySelector(".answerThird")
 const answerFourth = document.querySelector(".answerFourth")
 const answerFifth = document.querySelector(".answerFifth")
 const thirdSectionAnimationFirst=document.querySelector(".thirdSectionAnimationFirst")
-
+const animationImgFirst=document.querySelector(".animationImgFirst")
+const thirdSectionAnimationSecond=document.querySelector(".thirdSectionAnimationSecond")
+const animationImgSecond=document.querySelector(".animationImgSecond")
+const thirdSectionAnimationThird=document.querySelector(".thirdSectionAnimationThird")
+const animationImgThird=document.querySelector(".animationImgThird")
+const thirdSectionAnimationFourth=document.querySelector(".thirdSectionAnimationFourth")
+const animationImgFourth=document.querySelector(".animationImgFourth")
+const changeImg = document.querySelector(".firstPhoneThirdSection")
 
 function questionFirstShow(){
     questionFirst.classList.toggle('active')
@@ -41,9 +48,175 @@ function questionFifthShow(){
     answerFifth.style.color='#ffffff'
 }
 
-function thirdSectionLightAnimation(){
-    thirdSectionAnimationFirst.classList.add('active')
+function thirdSectionLightAnimationFirst(){
+    thirdSectionAnimationFirst.classList.toggle('activethirdSectionAnimationFirst')
+    if (thirdSectionAnimationFirst.classList.contains('activethirdSectionAnimationFirst')){
+        animationImgFirst.src="./thirdSectionAnimationFirstLight.png"
+    }
+    else{
+        animationImgFirst.src="./thirdSectionAnimationFirst.png"
+    }
+
+    if(thirdSectionAnimationSecond.classList.contains('activethirdSectionAnimationSecond')){
+        thirdSectionAnimationSecond.classList.remove('activethirdSectionAnimationSecond')
+        if (thirdSectionAnimationSecond.classList.contains('activethirdSectionAnimationSecond')){
+            animationImgSecond.src="./thirdSectionAnimationSecondLight.png"
+        }
+        else{
+            animationImgSecond.src="./thirdSectionAnimationSecond.png"
+        }
+    }
+    else if(thirdSectionAnimationThird.classList.contains('activethirdSectionAnimationThird')){
+        thirdSectionAnimationThird.classList.remove('activethirdSectionAnimationThird')
+        if (thirdSectionAnimationThird.classList.contains('activethirdSectionAnimationThird')){
+            animationImgThird.src="./thirdSectionAnimationThirdLight.png"
+        }
+        else{
+            animationImgThird.src="./thirdSectionAnimationThird.png"
+        }
+    }
+    else if(thirdSectionAnimationFourth.classList.contains('activethirdSectionAnimationFourth')){
+        thirdSectionAnimationFourth.classList.remove('activethirdSectionAnimationFourth')
+        if (thirdSectionAnimationFourth.classList.contains('activethirdSectionAnimationFourth')){
+            animationImgFourth.src="./thirdSectionAnimationFourthLight.png"
+        }
+        else{
+            animationImgFourth.src="./thirdSectionAnimationFourth.png"
+        }
+    }
 }
+
+function thirdSectionLightAnimationSecond(){
+    thirdSectionAnimationSecond.classList.toggle('activethirdSectionAnimationSecond')
+    if (thirdSectionAnimationSecond.classList.contains('activethirdSectionAnimationSecond')){
+        animationImgSecond.src="./thirdSectionAnimationSecondLight.png"
+    }
+    else{
+        animationImgSecond.src="./thirdSectionAnimationSecond.png"
+    }
+
+    if(thirdSectionAnimationFirst.classList.contains('activethirdSectionAnimationFirst')){
+        thirdSectionAnimationFirst.classList.remove('activethirdSectionAnimationFirst')
+        if (thirdSectionAnimationFirst.classList.contains('activethirdSectionAnimationFirst')){
+            animationImgFirst.src="./thirdSectionAnimationFirstLight.png"
+        }
+        else{
+            animationImgFirst.src="./thirdSectionAnimationFirst.png"
+        }
+    }
+    else if(thirdSectionAnimationThird.classList.contains('activethirdSectionAnimationThird')){
+        thirdSectionAnimationThird.classList.remove('activethirdSectionAnimationThird')
+        if (thirdSectionAnimationThird.classList.contains('activethirdSectionAnimationThird')){
+            animationImgThird.src="./thirdSectionAnimationThirdLight.png"
+        }
+        else{
+            animationImgThird.src="./thirdSectionAnimationThird.png"
+        }
+    }
+    else if(thirdSectionAnimationFourth.classList.contains('activethirdSectionAnimationFourth')){
+        thirdSectionAnimationFourth.classList.remove('activethirdSectionAnimationFourth')
+        if (thirdSectionAnimationFourth.classList.contains('activethirdSectionAnimationFourth')){
+            animationImgFourth.src="./thirdSectionAnimationFourthLight.png"
+        }
+        else{
+            animationImgFourth.src="./thirdSectionAnimationFourth.png"
+        }
+    }
+}
+
+function thirdSectionLightAnimationThird(){
+    thirdSectionAnimationThird.classList.toggle('activethirdSectionAnimationThird')
+    if (thirdSectionAnimationThird.classList.contains('activethirdSectionAnimationThird')){
+        animationImgThird.src="./thirdSectionAnimationThirdLight.png"
+    }
+    else{
+        animationImgThird.src="./thirdSectionAnimationThird.png"
+    }
+
+    if(thirdSectionAnimationSecond.classList.contains('activethirdSectionAnimationSecond')){
+        thirdSectionAnimationSecond.classList.remove('activethirdSectionAnimationSecond')
+        if (thirdSectionAnimationSecond.classList.contains('activethirdSectionAnimationSecond')){
+            animationImgSecond.src="./thirdSectionAnimationSecondLight.png"
+        }
+        else{
+            animationImgSecond.src="./thirdSectionAnimationSecond.png"
+        }
+    }
+    else if(thirdSectionAnimationFirst.classList.contains('activethirdSectionAnimationFirst')){
+        thirdSectionAnimationFirst.classList.remove('activethirdSectionAnimationFirst')
+        if (thirdSectionAnimationFirst.classList.contains('activethirdSectionAnimationFirst')){
+            animationImgFirst.src="./thirdSectionAnimationFirstLight.png"
+        }
+        else{
+            animationImgFirst.src="./thirdSectionAnimationFirst.png"
+        }
+    }
+    else if(thirdSectionAnimationFourth.classList.contains('activethirdSectionAnimationFourth')){
+        thirdSectionAnimationFourth.classList.remove('activethirdSectionAnimationFourth')
+        if (thirdSectionAnimationFourth.classList.contains('activethirdSectionAnimationFourth')){
+            animationImgFourth.src="./thirdSectionAnimationFourthLight.png"
+        }
+        else{
+            animationImgFourth.src="./thirdSectionAnimationFourth.png"
+        }
+    }
+}
+
+function thirdSectionLightAnimationFourth(){
+    thirdSectionAnimationFourth.classList.toggle('activethirdSectionAnimationFourth')
+    if (thirdSectionAnimationFourth.classList.contains('activethirdSectionAnimationFourth')){
+        animationImgFourth.src="./thirdSectionAnimationFourthLight.png"
+    }
+    else{
+        animationImgFourth.src="./thirdSectionAnimationFourth.png"
+    }
+
+    if(thirdSectionAnimationSecond.classList.contains('activethirdSectionAnimationSecond')){
+        thirdSectionAnimationSecond.classList.remove('activethirdSectionAnimationSecond')
+        if (thirdSectionAnimationSecond.classList.contains('activethirdSectionAnimationSecond')){
+            animationImgSecond.src="./thirdSectionAnimationSecondLight.png"
+        }
+        else{
+            animationImgSecond.src="./thirdSectionAnimationSecond.png"
+        }
+    }
+    else if(thirdSectionAnimationThird.classList.contains('activethirdSectionAnimationThird')){
+        thirdSectionAnimationThird.classList.remove('activethirdSectionAnimationThird')
+        if (thirdSectionAnimationThird.classList.contains('activethirdSectionAnimationThird')){
+            animationImgThird.src="./thirdSectionAnimationThirdLight.png"
+        }
+        else{
+            animationImgThird.src="./thirdSectionAnimationThird.png"
+        }
+    }
+    else if(thirdSectionAnimationFirst.classList.contains('activethirdSectionAnimationFirst')){
+        thirdSectionAnimationFirst.classList.remove('activethirdSectionAnimationFirst')
+        if (thirdSectionAnimationFirst.classList.contains('activethirdSectionAnimationFirst')){
+            animationImgFirst.src="./thirdSectionAnimationFirstLight.png"
+        }
+        else{
+            animationImgFirst.src="./thirdSectionAnimationFirst.png"
+        }
+    }
+}
+
+
+const imgLinks = [
+    './firstPhoneThirdSection.png',
+    './secondPhoneThirdSection.png'
+];
+const delay = 5000;
+let currentIndex = 0;
+setInterval(function() {
+    changeImg.src = imgLinks[currentIndex];
+    currentIndex++;
+    if(currentIndex >= imgLinks.length) {
+        currentIndex = 0;
+    }
+}, delay);
+
+
+
 
 
 questionFirst.addEventListener('click',questionFirstShow)
@@ -51,6 +224,10 @@ questionSecond.addEventListener('click',questionSecondShow)
 questionThird.addEventListener('click',questionThirdShow)
 questionFourth.addEventListener('click',questionFourthShow)
 questionFifth.addEventListener('click',questionFifthShow)
-thirdSectionAnimationFirst.addEventListener('click',thirdSectionLightAnimation)
+thirdSectionAnimationFirst.addEventListener('click',thirdSectionLightAnimationFirst)
+thirdSectionAnimationSecond.addEventListener('click',thirdSectionLightAnimationSecond)
+thirdSectionAnimationThird.addEventListener('click',thirdSectionLightAnimationThird)
+thirdSectionAnimationFourth.addEventListener('click',thirdSectionLightAnimationFourth)
+
 
 
